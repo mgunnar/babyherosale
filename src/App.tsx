86e100958ba4536ab0e88eca869cb6e3d3c380a4
m <devmatheus@gmail.com>
2023-05-23 18:43:24 -0300

    corrige

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
 import React from "react";
-import "./App.css";
+import TextoPrincipal from "./TextoPrincipal";
 
 function App() {
   return (
-    <>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap"
-        rel="stylesheet"
-      />
-      <div className="App">
-        <header className="App-header"></header>
+    <TextoPrincipal />
 
-        <main>
-          <div id="root">
-            <div className="container">
-              <h1 className="title">babyhero.com.br</h1>
-              <p className="convincing-text">
-                Este domínio único está disponível para venda!
-              </p>
-              <p className="convincing-text">Entre em contato agora mesmo,</p>
-              <p className="contact-email">
-                envie um e-mail para: <br />
-                <a href="mailto:devmatheus@gmail.com">devmatheus@gmail.com</a>
-              </p>
-            </div>
-          </div>
-        </main>
-      </div>
-    </>
   );
 }
 
